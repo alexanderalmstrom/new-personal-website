790e46726a34bf0549f13864a155dd19424d4062
Alexander Almström <info@alexanderalmstrom.com>
2022-11-03 23:40:40 +0100

    Add prismic

---
+++ .slicemachine/assets/slices/TextBlock/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/TextBlock';
+
+export default {
+  title: 'slices/TextBlock'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{"text":[{"type":"paragraph","text":"Velit reprehenderit pariatur eu anim occaecat irure. Enim commodo laborum sint quis aute consequat. Occaecat aute in proident incididunt labore.","spans":[]}]}],"primary":{"title":[{"type":"heading1","text":"Agree","spans":[]}],"description":[{"type":"paragraph","text":"Ullamco sunt consequat do quis.","spans":[]}]},"slice_type":"text_block","id":"_Default"}} />
+_Default.storyName = ''
