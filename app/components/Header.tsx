4476deabcd60f1e4730f1354c564d75e8511b410
Alexander Almström <info@alexanderalmstrom.com>
2022-11-04 01:25:44 +0100

    List page links in header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -5,13 +5,16 @@ export default async function Header() {
   const pages = await client.getEntries({
     content_type: "page",
     select: ["fields.name", "fields.slug"],
+    order: "sys.createdAt",
   });
 
   return (
     <header>
       <nav>
         {pages.items.map((page) => (
-          <Link href={page.fields.slug}>{page.fields.name}</Link>
+          <Link href={page.fields.slug === "home" ? "/" : page.fields.slug}>
+            {page.fields.name}
+          </Link>
         ))}
       </nav>
     </header>
